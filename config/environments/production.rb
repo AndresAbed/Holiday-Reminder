393b7178f541290581acdc86b682336637fab8e1
AndresAbed <abedandres@gamil.com>
2014-07-29 05:51:30 -0300

    App url added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => 'http://sleepy-retreat-5778.herokuapp.com/'}
+  config.action_mailer.default_url_options = {:host => 'http://stark-forest-8850.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => "127.0.0.1",
   :port    => 25,
-  :domain  => 'http://sleepy-retreat-5778.herokuapp.com/'
+  :domain  => 'http://stark-forest-8850.herokuapp.com/'
 }
 
 end
