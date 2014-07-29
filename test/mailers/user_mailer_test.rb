4d2128c5f539e46a3521586399b68639b79a7499
AndresAbed <abedandres@gamil.com>
2014-07-29 05:34:25 -0300

    clean

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "reminder_notification" do
-    mail = UserMailer.reminder_notification
+    mail = UserMailer.send_notification
     assert_equal "Reminder notification", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
