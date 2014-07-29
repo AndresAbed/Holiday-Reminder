a0c502a1475794bb4af9219c29a3642353f44ad7
AndresAbed <abedandres@gamil.com>
2014-07-29 03:38:18 -0300

    Migration deleted

--- db/migrate/20140729024906_add_customers_to_holidays.rb
+++
@@ -1,9 +0,0 @@
-class AddCustomersToHolidays < ActiveRecord::Migration
-  def change
-  	add_column :holidays, :customer_name, :string
-  	add_column :holidays, :customer_country, :string
-  	add_column :holidays, :customer_province, :string
-  	add_column :holidays, :customer_email, :string
-  	remove_column :holidays, :customer_id, :integer
-  end
-end
