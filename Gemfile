325c9ee6377b6e21fb9bdba7a3f0b75c38e18e49
Andres Abed <abedandres@gmail.com>
2014-08-02 04:32:27 -0300

    Whenever gem added

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'devise'
 gem 'rails_12factor', group: :production
-gem 'rufus-scheduler'
+gem 'whenever', :require => false
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 	:address		=> 'smtp.sendgrid.net',
 	:port			=> '587',
 	:authentication => :plain,
-	:user_name		=> 'app27912672@heroku.com',
-	:password		=> 'a3nlvr3h',
+	:user_name		=> 'app28051528@heroku.com',
+	:password		=> 'ypw1wi1c',
 	:domain			=> 'heroku.com',
 	:enable_starttls_auto => true
 }
