7cfa60e96d00dfacce90147f0d5647b719194574
Andres Abed <abedandres@gmail.com>
2014-08-05 02:58:11 -0300

    .

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,17 @@
 worker_processes 3
-timeout 30
-preload_app true
-
-before_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
 after_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
   end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+end end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+end
+
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 end
