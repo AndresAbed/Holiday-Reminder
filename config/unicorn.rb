0c535d4d44ff28460642d44214314be2d4b740da
Andres Abed <abedandres@gmail.com>
2014-08-05 03:04:19 -0300

    .

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,12 +6,4 @@ after_fork do |server, worker|
   Sidekiq.configure_server do |config|
     config.redis = { :size => 5 }
   end
-end end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 end
